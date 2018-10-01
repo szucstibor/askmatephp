c99387ccbeadd53191d948668fecc48e671ad6ac
Szűcs Tibor <job.szucstibor@gmail.com>
2018-10-01 13:22:51 +0200

    Basic login page

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <body>
         <h2>Log in to askm8, m8</h2>
         <a href="index.php">Go back to the main page</a>
-        <form>
+        <form action="checklogin.php" method="post">
             Enter username: <input type="text" name="username" required="required"/><br/>
             Enter password: <input type="text" name="password" required="required"/><br/>
             <input type="submit" value="Login"/>
