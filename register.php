aee554a1e48aea55da6038381a04a629fbe4b02f
Szűcs Tibor <job.szucstibor@gmail.com>
2018-10-01 11:30:33 +0200

    basic registration page

---
+++ register.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Reg for askm8</title>
+    </head>
+    <body>
+        <h2>Registration page</h2>
+        <a href="index.php">Go back to the main page</a>
+        <form action="register.php", method="POST">
+            Enter username: <input type="text" name="username" required="required"/><br/>
+            Enter password: <input type="text" name="password" required="required"/><br/>
+            <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
+
