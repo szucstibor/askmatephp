85a105371255cac78165fdb97eb6f1624a9999bf
antalEhrbar <ehranti@gmail.comgit config --global user.name AntalEhrbar>
2018-10-03 11:01:15 +0200

    Reply created, Comment, Answer modified

---
+++ src/Comment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antalehrbar
+ * Date: 03/10/18
+ * Time: 10:52
+ */
+
+class Comment extends Reply
+{
+
+}
---
+++ src/Reply.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antalehrbar
+ * Date: 02/10/18
+ * Time: 13:47
+ */
+
+class Reply
+{
+    private $reply;
+    private $datePosted;
+    private $timePosted;
+    private $dateEdited;
+    private $timeEdited;
+    private $user;
+
+    /**
+     * Reply constructor.
+     * @param $reply
+     * @param $datePosted
+     * @param $timePosted
+     * @param $user
+     */
+    public function __construct($reply, $datePosted, $timePosted, $user)
+    {
+        $this->reply = $reply;
+        $this->datePosted = $datePosted;
+        $this->timePosted = $timePosted;
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatePosted()
+    {
+        return $this->datePosted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimePosted()
+    {
+        return $this->timePosted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateEdited()
+    {
+        return $this->dateEdited;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimeEdited()
+    {
+        return $this->timeEdited;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReply()
+    {
+        return $this->reply;
+    }
+
+    /**
+     * @param mixed $reply
+     */
+    public function setReply($reply)
+    {
+        $this->reply = $reply;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+}
