8725e736bb4e48496e6124d22169fb5adfe576b5
Szűcs Tibor <job.szucstibor@gmail.com>
2018-10-03 09:38:28 +0200

    Fixed database name and autologin implemented

--- src/register.php
+++ src/register.php
@@ -4,10 +4,11 @@
     </head>
     <body>
         <h2>Registration page</h2>
-        <a href="index.php">Go back to the main page</a>
+        <a href="/askmatephp">Go back to the main page</a>
         <form action="register.php", method="POST">
             Enter username: <input type="text" name="username" required="required"/><br/>
             Enter password: <input type="text" name="password" required="required"/><br/>
+            Enter email address: <input type="text", name="email" required="required"/><br/>
             <input type="submit" value="Register"/>
         </form>
     </body>
@@ -19,23 +20,31 @@ $link = mysqli_connect("localhost", "root", "", "ask_mate");
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = mysqli_real_escape_string($link , $_POST["username"]);
     $password = mysqli_real_escape_string($link, $_POST["password"]);
+    $email = mysqli_real_escape_string($link, $_POST["email"]);
     $bool = true;
 
     mysqli_select_db($link, "ask_mate");
-    $query = mysqli_query($link, "SELECT * FROM users");
+    $query = mysqli_query($link, "SELECT * FROM user");
     while($row = mysqli_fetch_array($query)){
         $table_users = $row['username'];
+        $table_email = $row['email'];
         if ($username == $table_users){
             $bool = false;
-                Print '<script>alert("Username has been taken");</script>';
-                Print '<script>window.location.assign("register.php")</script>';
+            Print '<script>alert("Username has been taken");</script>';
+            Print '<script>window.location.assign("src/register.php")</script>';
+        } elseif ($table_email == $email) {
+            $bool = false;
+            Print '<script>alert("Email address is already in use");</script>';
+            Print '<script>window.location.assign("src/register.php")</script>';
         }
+
     }
 
     if ($bool){
-        mysqli_query($link, "INSERT INTO users (username, password) VALUES ('$username', '$password') ");
-        Print '<script>alert("Successfully registered!")</script>';
-        Print '<script>window.location.assign("login.php")</script>';
+        mysqli_query($link, "INSERT INTO user (username, password, email) VALUES ('$username', '$password', '$email') ");
+        Print '<script>alert("Successfully registered! You will be redirected to the home page.")</script>';
+        $_SESSION['user'] = $username;
+        Print '<script>window.location.assign("home.php")</script>';
     }
 }
 ?>
