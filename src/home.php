d0acb87c9afe2bfd694dea1323165e897747b279
Szűcs Tibor <job.szucstibor@gmail.com>
2018-10-04 10:57:58 +0200

    Displaying questions

--- src/home.php
+++ src/home.php
@@ -1,47 +1,54 @@
 <html>
-    <head>
-        <title>
-            AskM8
-        </title>
-    </head>
-    <body>
-         <?php
-            session_start();
-                echo $_SESSION['user'];
-             ?>
-        <a href="logout.php">Logout</a>
-        <br>
-         <form action="add.php" method="post">
-             Ask a question (255 characters):
-             <br/>
-             <textarea placeholder="Question Title..." name="title" maxlength="255" required="required"></textarea>
-             <br/>
-             Detailed description (1000 characters):
-             <br/>
-             <textarea placeholder="Question goes here..." name="description" maxlength="1000" required="required"></textarea>
-             <input type="submit" value="Submit question" />
-         </form>
-         <h2>My questions</h2>
-         <table>
-             <tr>
-                 <td>Title</td>
-                 <td>Date</td>
-                 <td>Amount of answers</td>
-                 <td>User</td>
-             </tr>
-             <?php
-                require ("Link.php");
-                $query = mysqli_query($link, "SELECT * FROM question");
-                while($row = mysqli_fetch_array($query))
-                {
-                    echo "
-                    <tr>";$row['title'];"</tr>
-                    <tr>";$row['date_posted'];"</tr>
-                    <tr>";"</tr>
-                    <tr>";$row['user_id'];"</tr>
-                    ";
-                }
-             ?>
-         </table>
-    </body>
+<head>
+    <title>
+        AskM8
+    </title>
+</head>
+<body>
+<?php
+session_start();
+echo $_SESSION['user'];
+?>
+<a href="logout.php">Logout</a>
+<br>
+<form action="add.php" method="post">
+    Ask a question (255 characters):
+    <br/>
+    <textarea placeholder="Question Title..." name="title" maxlength="255" required="required"></textarea>
+    <br/>
+    Detailed description (1000 characters):
+    <br/>
+    <textarea placeholder="Question goes here..." name="description" maxlength="1000" required="required"></textarea>
+    <input type="submit" value="Submit question"/>
+</form>
+<h2>My questions</h2>
+<table>
+    <tr>
+        <td align="center">Title</td>
+        <td align="center">Date</td>
+        <td align="center">Amount of answers</td>
+        <td align="center">User</td>
+    </tr>
+    <?php
+    require("Link.php");
+    $query = mysqli_query($link, "SELECT * FROM question");
+    while ($row = mysqli_fetch_array($query)) {
+        Print "<tr>";
+        Print "<td align='center'>" . $row['title'];
+        "</td>";
+        Print "<td align='center'>" . $row['date_posted'];
+        "</td>";
+        Print "<td align='center'>X</td>";
+        $id = $row["user_id"];
+        $user = mysqli_query($link, "SELECT * FROM user WHERE id=$id");
+    while ($users = mysqli_fetch_assoc($user)) {
+        $username = $users['username'];
+    }
+        Print "<td align='center'>" . $username;
+        "</td>";
+        Print "</tr>";
+    }
+    ?>
+</table>
+</body>
 </html>
