74f8dabab51104694a83ca8c9bf39e2a14377a8f
antalEhrbar <ehranti@gmail.comgit config --global user.name AntalEhrbar>
2018-10-04 10:44:55 +0200

    saving question to db

--- src/add.php
+++ src/add.php
@@ -1,12 +1,24 @@
 <?php
+require ("Link.php");
 session_start();
 if($_SESSION['user']){
 }
 else{
-    header("location:index.php");
+    header("location:home.php");
 }
 
-$time = strftime("%X"); //time
-$date = strftime("%B %d, %Y"); //date
 
 Print "$time - $date";
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $user = $_SESSION['user_id'];
+    $title = mysqli_real_escape_string($link, $_POST['title']);
+    $description = mysqli_real_escape_string($link, $_POST['description']);
+    $time = strftime("%X"); //time
+    $date = strftime("%B %d, %Y"); //date
+    mysqli_query($link, "INSERT INTO question (user_id, title, description, date_posted, time_posted) 
+                  VALUES ('$user', '$title', '$description', '$date', '$time')");
+    header("location: home.php");
+
+}
